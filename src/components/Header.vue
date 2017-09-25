7b26b345da7fb80a3bdb985cfdcd0657471fb028
Jojnas <jan.stancek49@gmail.com>
2017-09-25 13:21:02 +0200

    adding portfolio view and handlers

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,13 +10,14 @@
                     <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
                     <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
                 </ul>
+                <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">End Day</a></li>
-                    <li class="dropdown">
+                    <li><a href="#" @click="endDay">End Day</a></li>
+                    <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Save Data</a></li>
-                            <li><a href="#">Load Data</a></li>
+                            <li><a href="#" @click="saveData">Save Data</a></li>
+                            <li><a href="#" @click="loadData">Load Data</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -24,3 +25,39 @@
         </div><!-- /.container-fluid -->
     </nav>
 </template>
+
+<script>
+    import {mapActions} from 'vuex';
+
+    export default {
+        data() {
+            return {
+                isDropdownOpen: false
+            }
+        },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            }
+        },
+        methods: {
+            ...mapActions([
+                'randomizeStocks'
+            ]),
+            endDay() {
+                this.randomizeStocks();
+            },
+            saveData() {
+                const data = {
+                    funds: this.$store.getters.funds,
+                    stockPortfolio: this.$store.getters.stockPortfolio,
+                    stocks: this.$store.getters.stocks
+                };
+                this.$http.put('data.json', data);
+            },
+            loadData() {
+
+            }
+        }
+    }
+</script>
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -6,15 +6,21 @@
             </div>
             <div class="panel-body">
                 <div class="pull-left">
-                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
+                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity" :class="{danger: insufficientQuantity}">
                 </div>
                 <div class="pull-right">
-                    <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || Number.isInteger(quantity)">Buy</button>
+                    <button class="btn btn-success" @click="buyStock" :disabled="insufficientFunds || quantity <= 0 || Number.isInteger(quantity)">{{ insufficientFunds ? 'Insufficient Funds' : 'Buy' }}</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 
 <script>
     export default {
@@ -24,6 +30,14 @@
                 quantity: 0
             }
         },
+        computed: {
+          funds() {
+              return this.$store.getters.funds;
+          },
+          insufficientFunds() {
+              return this.quantity * this.stock.price > this.funds;
+          }
+        },
         methods: {
             buyStock() {
                 const order = {
@@ -31,7 +45,7 @@
                     stockPrice: this.stock.price,
                     quantity: this.quantity
                 };
-                console.log(order);
+                this.$store.dispatch('buyStock', order);
                 this.quantity = 0;
             }
         }
